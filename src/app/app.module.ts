3e99ed567c7e92e21021950107246438078521d7
Yazmin Suarez <yazminsp@yahoo.com>
2021-12-07 23:32:19 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Contadormodule } from './contador/contador.module';
+
+import { Heroesmodule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Heroesmodule,
+    Contadormodule
   ],
   providers: [],
   bootstrap: [AppComponent]
